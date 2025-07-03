904170a1e1573d716ae7e1953138369eb7c07755
Mohammed Faizan <mohdfzn889@gmail.com>
2025-07-03 21:13:30 +0530

    admin view

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,15 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
-
 from django.conf.urls.static import static
+
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "yourpassword")
+        return HttpResponse("Superuser created!")
+    else:
+        return HttpResponse("Superuser already exists!")
 
 
 
 urlpatterns = [
 
 
-
+    path('create-admin/', create_admin),
     # Admin url
     
     path('admin/', admin.site.urls),
